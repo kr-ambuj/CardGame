a621951cd9acd5ce32b9ba946ea63a131860f3d2
Kumar Ambuj <kumar.ambuj@hotmail.com>
2021-01-20 22:56:20 +0530

    Add project files.

---
+++ CardGame/CardDeck.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class CardDeck
+    {
+        string[] valueSet = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+        string[] suitsSet = { "Clubs", "Hearts", "Spades", "Diamond" };
+        List<string> cardDeckSet;
+
+        public CardDeck()
+        {
+            cardDeckSet = new List<string>();
+            CreateDeck();
+        }
+
+        //Creating a deck.
+        private void CreateDeck()
+        {
+            foreach (string suite in suitsSet)
+            {
+                foreach(string value in valueSet)
+                {
+                    cardDeckSet.Add($"{value} of {suite}");
+                }
+            }
+            ShuffleDeck();
+        }
+
+        /// <summary>
+        /// Shuffling the current deck.
+        /// </summary>
+        public void ShuffleDeck()
+        {
+            Random random = new Random();
+            int deckSize = cardDeckSet.Count;
+            string[] tempDeck = cardDeckSet.ToArray();
+            cardDeckSet.Clear();
+            while(true)
+            {
+                int pos = random.Next(tempDeck.Length);
+                string card = tempDeck[pos];
+                if (!cardDeckSet.Contains(card))
+                {
+                    cardDeckSet.Add(card);
+                    if (cardDeckSet.Count == deckSize)
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Getting the top carrd from the Deck.
+        /// </summary>
+        /// <returns></returns>
+        public string GetTopCard()
+        {
+            string value = cardDeckSet.First();
+            cardDeckSet.Remove(value);
+            return value;
+        }
+    }
+}
---
+++ CardGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Program
+    {
+        public static CardDeck cardDeck;
+        static void Main(string[] args)
+        {
+            cardDeck = new CardDeck();
+            DisplayPart();
+        }
+
+        /// <summary>
+        /// Displaying the messages for input and capturing the input.
+        /// </summary>
+        private static void DisplayPart()
+        {
+            Console.WriteLine("Press 1 to Play a Card:");
+            Console.WriteLine("Press 2 to Shuffle the Deck:");
+            Console.WriteLine("Press 3 to Restart the Game:");
+            string input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    Console.WriteLine(cardDeck.GetTopCard());
+                    Console.WriteLine();
+                    break;
+                case "2":
+                    cardDeck.ShuffleDeck();
+                    Console.WriteLine("Current Deck Shuffled.");
+                    Console.WriteLine();
+                    break;
+                case "3":
+                    cardDeck = new CardDeck();
+                    Console.WriteLine("New Deck Created.");
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("Please choose a correct option from given list!!");
+                    break;
+            }
+            DisplayPart();
+        }
+    }
+}
